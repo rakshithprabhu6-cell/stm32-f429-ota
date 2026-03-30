5304b89c2472f57a04d3953ad3893e30525dbb0d
rakshithprabhu6-cell <rakshithprabhu6@gmail.com>
2026-03-30 16:20:08 +0530

    update 11 class with invalid sample

--- train1.py
+++ train1.py
@@ -1,6 +1,10 @@
 import os
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', errors='replace')
 import numpy as np
 import tensorflow as tf
 from pathlib import Path
